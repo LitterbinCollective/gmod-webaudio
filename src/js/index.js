0b96ab8719e0eed1f070705d316c34ee0316658c
be195 <ilya.wicopee@gmail.com>
2022-06-30 00:25:59 +0300

    js: add polyfill, add a Context class

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log('hello, world!');
+import './polyfill';
+
+function Context() {
+	this.audio = new AudioContext();
+	this.gain = this.audio.createGain();
+	this.compressor = this.audio.createDynamicsCompressor();
+
+	console.log('hoii!');
+}
+
+window.$ctx = new Context();
