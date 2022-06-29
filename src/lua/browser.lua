87036723536afabfd9872bef1db6c8faee88d9c6
be195 <ilya.wicopee@gmail.com>
2022-06-30 01:09:08 +0300

    lua: organize browser.lua

--- src/lua/browser.lua
+++ src/lua/browser.lua
@@ -3,39 +3,59 @@ local jsCode = require("browserjs")
 local BrowserController = {}
 BrowserController._M = BrowserController
 BrowserController._NAME = "BrowserController"
-BrowserController.Callbacks = {}
-
-function BrowserController:Log(...)
-	MsgC(Color(108, 216, 45), "[WebAudio/BrowserController] ") print(...)
-end
-
-function BrowserController:Destroy()
-	self:Log("Destroying...")
-	self._DHTML:Remove()
-
-	self.Destroyed = true
-end
-
-function BrowserController:SetVolume(volume)
-	self:RunJavascript(("$ctx.gain.gain.value = %d;"):format(math.Clamp(volume, 0, 1)))
-end
-
-function BrowserController:Register(nodeName, callback)
-
-end
-
-local DHTML = vgui.Create("DHTML")
-DHTML:SetSize(8, 8)
-DHTML:SetAlpha(0)
-DHTML:ParentToHUD()
-DHTML:SetHTML("<!DOCTYPE html><html></html>")
-DHTML.BrowserController = BrowserController
-
-function DHTML:ConsoleMessage(text)
-	self.BrowserController:Log(text)
-end
 
 do
+	BrowserController.Callbacks = {}
+
+	function BrowserController:Log(...)
+		MsgC(Color(108, 216, 45), "[WebAudio/BrowserController] ") print(...)
+	end
+
+	function BrowserController:Destroy()
+		self:Log("Destroying...")
+		self._DHTML:Remove()
+
+		self.Destroyed = true
+	end
+
+	function BrowserController:_SafetyCheck()
+		if not self.DHTML then return error("browser not initialized yet") end
+		if self.Destroyed then
+			return error("cannot call functions on a destroyed BrowserController")
+		end
+	end
+
+	function BrowserController:SetVolume(volume)
+		self:_SafetyCheck()
+		self.DHTML:RunJavascript(("$ctx.gain.gain.value = %d;"):format(math.Clamp(volume, 0, 1)))
+	end
+
+	function BrowserController:Register(nodeName, callback)
+		self:_SafetyCheck()
+	end
+end
+
+do -- DHTML
+	local DHTML = vgui.Create("DHTML")
+	DHTML:SetSize(8, 8)
+	DHTML:SetAlpha(0)
+	DHTML:ParentToHUD()
+	DHTML:SetHTML("<!DOCTYPE html><html></html>")
+	DHTML.BrowserController = BrowserController
+
+	function DHTML:ConsoleMessage(text)
+		self.BrowserController:Log(text)
+	end
+
+	function DHTML:OnAutoplayFixed()
+		self.BrowserController:Log("Autoplay fixed, running JavaScript code...")
+		DHTML:RunJavascript(jsCode)
+	end
+
+	BrowserController._DHTML = DHTML
+end
+
+do -- Autoplay fix
 	local STATE_CLICK = 1
 	local STATE_RELEASE = 2
 
@@ -77,12 +97,5 @@ do
 		self:FixAutoplay()
 	end
 end
-
-function DHTML:OnAutoplayFixed()
-	self.BrowserController:Log("Autoplay fixed, running JavaScript code...")
-	DHTML:RunJavascript(jsCode)
-end
-
-BrowserController._DHTML = DHTML
 
 return BrowserController
