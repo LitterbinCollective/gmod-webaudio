ae5fb86e78f2fd3481e9a53489f7f20c0a036bb8
be195 <ilya.wicopee@gmail.com>
2022-06-30 12:53:28 +0300

    lua: import nodes.SourceNode

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -2,6 +2,9 @@ local WebAudio = {}
 WebAudio._M = WebAudio
 WebAudio._NAME = "WebAudio"
 
+WebAudio.Constructors = {
+	SourceNode = require("nodes.SourceNode")
+}
 WebAudio.BrowserController = require("browser")
 
 return WebAudio
